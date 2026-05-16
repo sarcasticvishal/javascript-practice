877ee3742cf693bb2686ded08ed3faaba1a008ac
vishal <140131427+sarcasticvishal@users.noreply.github.com>
2026-05-16 21:13:25 +0530

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,15 @@
+let score =100;
+let scorePass=true;
+if (score >=100 && scorePass){
+    console.log("you are pass")
+}else{
+    console.log("you are fail")
+}
+
+let username = "admin";
+let password = "1234";
+if (username === "admin" && password === "1234"){
+    console.log("login successful")
+}else{
+    console.log("eroor")
+}
